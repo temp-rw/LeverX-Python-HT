0beede133b1263225a16526456d4f235135f4726
Artiom Daniuk <tema200100@gmail.com>
2020-12-01 03:07:08 +0300

    Rewrite Version class using math algorithm to compare version identifier.

--- Task2/task_2.py
+++ Task2/task_2.py
@@ -1,111 +1,110 @@
-import re
+from typing import List
+from functools import total_ordering
 
 
+@total_ordering
 class Version:
-    RE_EXPR = r"^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)([a-z]*)" \
-              r"(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?"
+
+    # Weights for alphabetic version identifiers
+    # May be expanded.
+    ALPHA_VERSION_WEIGHTS = {"alpha": 1, "beta": 2, "rc": 3}
 
     def __init__(self, version: str):
         self.string_version = version
-        version = re.split(Version.RE_EXPR, version)
-        self.tuple_version = tuple(version[1:-1:1])
+        if len(self.string_version.split("-")) > 1:
+            self.head, self.tail = self.string_version.split("-")
+        else:
+            self.head, self.tail = self.string_version.split("-")[0], ""
 
-    def __ne__(self, other):
-        return not self == other
+        self.head = self.head.split(".")
+        self.tail = self.tail.split(".")
 
     def __eq__(self, other):
         if len(self.string_version) != len(other.string_version):
             return False
         else:
-            for left, right in zip(self.tuple_version, other.tuple_version):
-                if left != right:
-                    return False
+            return self.string_version == other.string_version
 
     def __lt__(self, other):
-        # major, minor, patch comparison
-        for left, right in zip(self.tuple_version[:4], other.tuple_version[:4]):
-            if left < right:
-                return True
-            elif left > right:
-                return False
+        """
+        Compare left-side value with right-side value.
+        Every version identifier gets it's own weight in strings_to_coefficients function
+        then compare them using this function.
+        Comparing algorithm:
+        Every next version identifier gets it's weight divided by 10 and normalized by using biggest
+        coefficient in comparing pair.
+        :param other:
+        :return:
+        """
+        right_sum, left_sum = 0, 0
+        coef = 1
 
-        # pre-release tail comparison
-        for left, right in zip(self.tuple_version[4:], other.tuple_version[4:]):
-            if left is None:
-                return False
-            elif right is None:
-                return True
+        left_side = self.strings_to_coefficients(self.head)
+        right_side = self.strings_to_coefficients(other.head)
+
+        for left_element, right_element in zip(left_side, right_side):
+            if max(left_element, right_element) > 0:
+                norm = max(left_element, right_element)
             else:
-                inner_left, inner_right = left.split('.'), right.split('.')
-                for left_item, right_item in zip(inner_left, inner_right):
-                    if left_item.isnumeric() and right_item.isnumeric():
-                        if left_item < right_item:
-                            return True
-                        elif left_item > right_item:
-                            return False
-                    elif left_item.isnumeric() or right_item.isnumeric():
-                        return left_item.isnumeric()
-                    elif left_item.isalpha() and right_item.isalpha():
-                        if left_item == right_item:
-                            continue
-                        else:
-                            return left_item < right_item
+                norm = 1.0
 
-                if len(inner_left) < len(inner_right):
-                    return True
-                elif len(inner_left) > len(inner_right):
-                    return False
+            left_sum += (left_element / norm) * coef
+            right_sum += (right_element / norm) * coef
+            coef /= 10.0
+            if right_sum != left_sum:
+                return left_sum < right_sum
 
-    def __le__(self, other):
-        if self.__lt__(other):
-            return True
-        elif self.__eq__(other):
-            return True
+        if len(left_side) != len(right_side):
+            return len(left_side) < len(right_side)
 
-        return False
+        right_sum, left_sum = 0, 0
+        coef = 1
 
-    def __gt__(self, other):
-        # major, minor, patch comparison
-        for left, right in zip(self.tuple_version[:4], other.tuple_version[:4]):
-            if left > right:
-                return True
-            elif left < right:
-                return False
+        left_side = self.strings_to_coefficients(self.tail)
+        right_side = self.strings_to_coefficients(other.tail)
 
-        # pre-release tail comparison
-        for left, right in zip(self.tuple_version[4:], other.tuple_version[4:]):
-            if left is None:
-                return True
-            elif right is None:
-                return False
-            else:
-                inner_left, inner_right = left.split('.'), right.split('.')
-                for left_item, right_item in zip(inner_left, inner_right):
-                    if left_item.isnumeric() and right_item.isnumeric():
-                        if left_item > right_item:
-                            return True
-                        elif left_item < right_item:
-                            return False
-                    elif left_item.isnumeric() or right_item.isnumeric():
-                        return left_item.isalpha()
-                    elif left_item.isalpha() and right_item.isalpha():
-                        if left_item == right_item:
-                            continue
-                        else:
-                            return left_item > right_item
+        if len(left_side) == 0 or len(right_side) == 0:
+            return len(left_side) > len(right_side)
 
-                if len(inner_left) > len(inner_right):
-                    return True
-                elif len(inner_left) < len(inner_right):
-                    return False
+        for left_element, right_element in zip(left_side, right_side):
+            norm = max(left_element, right_element)
+            left_sum += (left_element / norm) * coef
+            right_sum += (right_element / norm) * coef
+            coef /= 10.0
+            if right_sum != left_sum:
+                return left_sum < right_sum
 
-    def __ge__(self, other):
-        if self.__gt__(other):
-            return True
-        elif self.__eq__(other):
-            return True
+        return len(left_side) < len(right_side)
 
-        return False
+    @staticmethod
+    def strings_to_coefficients(strings: List[str]) -> List[int]:
+        """
+        Takes list of splitted version string, then gives weight to each element according to the rule:
+        1) integers are casted from string using int(),
+        2) alpha, beta and etc are converted using ALPHA_VERSION_WEIGHTS dictionary,
+        3) every single letter gets it's code according to ASCII table.
+        :param strings: splitted version string
+        :return list: List of corresponding coefficients
+        """
+        coefficient_list = []
+        for string in strings:
+            if string.isalpha():
+                coefficient_list.append(Version.ALPHA_VERSION_WEIGHTS.get(string) * 1000)
+            elif string.isnumeric():
+                coefficient_list.append(int(string))
+            elif string.isalnum():
+                num = []
+                alpha = []
+                for symbol in string:
+                    if symbol.isnumeric():
+                        num.append(symbol)
+                    else:
+                        alpha.append(ord(symbol))
+
+                num = "".join(num)
+                num = int(num)
+                coefficient_list.extend([num, alpha])
+        return coefficient_list
 
     def __str__(self):
         return self.string_version
@@ -119,7 +118,8 @@ def main():
         ("1.1.0-alpha", "1.2.0-alpha.1"),
         ("1.0.1b", "1.0.10-alpha.beta"),
         ("1.0.0-rc.1", "1.0.0"),
-        ("1.0.0-alpha.alpha.alpha", "1.0.0-alpha.alpha.beta")
+        ("1.0.0-alpha.alpha.alpha", "1.0.0-alpha.alpha.beta"),
+        ("1.0.0-alpha.10", "1.0.0-alpha.alpha"),
     ]
     for version_1, version_2 in to_test:
         assert Version(version_1) < Version(version_2), "le failed"
