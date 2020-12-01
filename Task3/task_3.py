5defb3e3c0248d7966592aa32bfd6f961b9fa1ca
Artiom Daniuk <tema200100@gmail.com>
2020-12-01 23:33:33 +0300

    Add thread_locker decorator. Wrap function with thread_locker.

--- Task3/task_3.py
+++ Task3/task_3.py
@@ -1,21 +1,38 @@
-from concurrent.futures import ThreadPoolExecutor
+from threading import Thread, Lock
 
 
-def function(arg):
-    a = 0
+a = 0
+
+
+def thread_locker(func):
+    lock = Lock()
+
+    def wrapper(*args):
+        func(*args, lock)
+
+    return wrapper
+
+
+@thread_locker
+def function(arg, lock):
+    global a
     for _ in range(arg):
+        lock.acquire()
         a += 1
-    return a
+        lock.release()
 
 
 def main():
     threads = []
-    executor = ThreadPoolExecutor(max_workers=5)
 
     for i in range(5):
-        thread = executor.submit(function, 1000000)
-
+        thread = Thread(target=function, args=(1000000, ))
+        thread.start()
         threads.append(thread)
 
-    a = sum([t.result() for t in threads])
+    [t.join() for t in threads]
     print("----------------------", a)  # 5000000
+
+
+if __name__ == '__main__':
+    main()
